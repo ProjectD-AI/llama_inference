5b00c6296d457ebc03ebc8346fa89682f68f842a
fengyh3 <648518051@qq.com>
2023-04-23 00:20:25 +0800

    update

--- generate.py
+++ generate.py
@@ -78,9 +78,7 @@ class LmGeneration:
         batch = len(prompts)
         assert batch <= args.batch_size
 
-        # prompt_tokens = [args.tokenizer.encode(x, bos=True, eos=False) for x in prompts]
-        prompt_tokens = [args.tokenizer.encode(x, bos=True, eos=True) for x in prompts]
-        each_seq_length = [len(p) for p in prompt_tokens]
+        prompt_tokens = [args.tokenizer.encode(x, bos=True, eos=False) for x in prompts]
 
         min_prompt_len = min([len(x) for x in prompt_tokens])
         # max_prompt_len = max([len(x) for x in prompt_tokens])
@@ -119,9 +117,6 @@ class LmGeneration:
                 # remove eos examples.
                 continue_exsample = []
                 for i, t in enumerate(tokens.tolist()):
-                    if cur_pos == each_seq_length[i]:
-                        continue_exsample.append(i)
-                        continue
                     try:
                         t.index(self.tokenizer.eos_id)
                     except ValueError:
@@ -140,7 +135,7 @@ class LmGeneration:
             t = t[: args.seq_length]
             try:
                 t = t[: t.index(self.tokenizer.pad_id)]
-                t = t[: t.index(self.tokenizer.eos_id, each_seq_length[i])]
+                t = t[: t.index(self.tokenizer.eos_id)]
             except ValueError:
                 pass
             decoder.append(self.tokenizer.decode(t))
